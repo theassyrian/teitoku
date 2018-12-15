7356fe3c70f8d4e52ef150548845d289b56f41a5
Ridho Pratama <p.ridho@yahoo.co.id>
2018-12-16 00:00:51 +0700

    Added basic mockup

---
+++ mockup/initialization.py
@@ -0,0 +1,14 @@
+from teitoku import Teitoku
+from teitoku.gateway import LINE
+
+# app initialization
+app = Teitoku(__name__)
+line_gateway = LINE(CHANNEL_SECRET, CHANNEL_ACCESS_TOKEN)
+
+app.add_gateway(line_gateway)
+
+
+@app.command("!call")
+def command_call():
+    res.add_text("Active")
+    res.reply()
