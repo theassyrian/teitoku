495098ad2076b8b082e06273be735872636fce5a
Ridho Pratama <p.ridho@yahoo.co.id>
2018-12-17 13:17:05 +0700

    Added flask and line bot handler

--- teitoku/gateway/line_gateway.py
+++ teitoku/gateway/line_gateway.py
@@ -1,4 +1,14 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
 from .base_gateway import Gateway
 
 
@@ -12,10 +22,34 @@ class LineGateway(Gateway):
         self.webhook_suffix = webhook_suffix
         self.host = host
 
+        self.line_bot_api = LineBotApi(self.channel_access_token)
+        self.webhook_handler = WebhookHandler(self.channel_secret)
+
         self.app = Flask("line_gateway")
+
+        @self.app.route("/{}".format(webhook_suffix), methods=['POST', 'GET'])
+        def webhook_callback():
+            # get X-Line-Signature header value
+            signature = request.headers['X-Line-Signature']
+
+            # get request body as text
+            body = request.get_data(as_text=True)
+            self.app.logger.info("Request body: " + body)
+
+            # handle webhook body
+            try:
+                self.webhook_handler.handle(body, signature)
+            except InvalidSignatureError:
+                abort(400)
+
+            return 'OK'
+
+        @self.webhook_handler.add(MessageEvent)
+        def handle_message(event):
+            print(event)
 
     def run(self):
         print("Started line webhook on http://{}:{}/{}".format(
               self.host, self.port, self.webhook_suffix))
 
-        self.app.run(self.host, self.port)
+        self.app.run(self.host, self.port, debug=False)
